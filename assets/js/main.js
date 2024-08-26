496185b332c8c500855707ad95516748d5d54dbf
IntenicsIt1 <intenics.it1@sdplweb.com>
2024-08-26 15:42:08 +0530

    Why Choose Us Finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,70 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
-
 /*=============== REMOVE MENU MOBILE ===============*/
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+const navLink = document.querySelectorAll(".nav_link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav_link, we remove the show-menu class navMenu.classList.remove('show-menu')
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/* ==== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrolly >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
+const popularSwiper = new Swiper(".popular__content", {
+  // Optional parameters
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== CHOOSE FAQ ===============*/
+const faqItems = document.querySelectorAll(".choose__faq-item");
+// 1. Select each item
+faqItems.forEach((item) => {
+  const faqHeader = item.querySelector(".choose__faq-header");
+  // 2. Select each button click
+  faqHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".faq-open");
+    toggleItem(item);
+    if (openItem && openItem != item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+// 3. Create function to display the content
+const toggleItem = (item) => {
+  // 3.1. Select each faq content
+  const faqContent = item.querySelector(".choose__faq-content");
+
+  if (item.classList.contains("faq-open")) {
+    faqContent.removeAttribute("style");
+    item.classList.remove("faq-open");
+  } else {
+    faqContent.style.height = faqContent.scrollHeight + "px";
+    item.classList.add("faq-open");
+  }
+};
 
 /*=============== SHOW SCROLL UP ===============*/
 
